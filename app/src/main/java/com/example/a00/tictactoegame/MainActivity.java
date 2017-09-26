8c106c2febc5bd869508183702aee1ded70b94c0
premus <premusvitka@gmail.com>
2017-09-25 23:42:09 -0400

    add onClickListener to MainActivity

--- app/src/main/java/com/example/a00/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/a00/tictactoegame/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.a00.tictactoegame;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.id;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     static String x;
     static String o;
     static String[] List;
@@ -16,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         List = new String[9];
         x = "x";
         o = "o";
+
     }
+
+    @Override
+    public void onClick(View view) {
+        int buttonId = view.getId();
+        Button button = (Button)findViewById(buttonId);
+        button.setAlpha(1.0f);
+    }
+
 }
