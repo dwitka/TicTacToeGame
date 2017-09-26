4fd41f297a9012b8b0bd0770eefde63fdb9e4450
premus <premusvitka@gmail.com>
2017-09-25 21:38:21 -0400

    add fields to MainActivity

--- app/src/main/java/com/example/a00/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/a00/tictactoegame/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    static String x;
+    static String o;
+    static String[] List;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List = new String[9];
+        x = "x";
+        o = "o";
     }
 }
